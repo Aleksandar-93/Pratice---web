2f9e26a08f52aea12e24b4935a472f8f1c75baf8
Aleksandar-93 <aleksandar.93m@gmail.com>
2021-01-26 13:51:33 +0100

    on click hide current item

--- To Do list/script.js
+++ To Do list/script.js
@@ -7,14 +7,20 @@ for(let i = 0; i < myList.length; i++){
     span.appendChild(text);
     myList[i].appendChild(span);
 }
-
-
-
 // Click on a close button to hide the current list item
-
+    const close = document.getElementsByClassName('close');
+    for(let i = 0; i < close.length; i++){
+        close[i].onclick = function() {
+            const div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
 
 
 // Add a "checked" symbol when clicking on a list item
+
+
+
 
 // Create a new list item when clicking on the "Add" button
 
