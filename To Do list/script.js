b5be719c95c6506b7da92d583e592abfe8ef8895
Aleksandar-93 <aleksandar.93m@gmail.com>
2021-01-26 13:41:19 +0100

    create close button

---
+++ To Do list/script.js
@@ -0,0 +1,21 @@
+// Create a "close" button and append it to each list item
+const myList = document.getElementsByTagName('LI');
+for(let i = 0; i < myList.length; i++){
+    const span = document.createElement('SPAN');
+    const text = document.createTextNode('\u00D7');
+    span.className = 'close';
+    span.appendChild(text);
+    myList[i].appendChild(span);
+}
+
+
+
+// Click on a close button to hide the current list item
+
+
+
+// Add a "checked" symbol when clicking on a list item
+
+// Create a new list item when clicking on the "Add" button
+
+
